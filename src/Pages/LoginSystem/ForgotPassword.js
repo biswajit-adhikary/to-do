1ee3fd54605338bad4819bc3bd43361ab09905f1
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-19 01:09:34 +0600

    Project Bootstrapping Done

---
+++ src/Pages/LoginSystem/ForgotPassword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPassword = () => {
+    return (
+        <div>
+            <h2>This is forgot password</h2>
+        </div>
+    );
+};
+
+export default ForgotPassword;
